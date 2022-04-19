ccebd670c34363a8cb12ee2f0b99b69ebf158262
kkkb1114 <kikkb1114@gmail.com>
2022-04-20 07:49:50 +0900

    0420

---
+++ app/src/main/java/com/example/michaelclone/MainRecord/ApRv_MainRecordPage.java
@@ -0,0 +1,31 @@
+package com.example.michaelclone.MainRecord;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ApRv_MainRecordPage extends RecyclerView.Adapter<ApRv_MainRecordPage.ViewHolder> {
+    @NonNull
+    @Override
+    public ApRv_MainRecordPage.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ApRv_MainRecordPage.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
